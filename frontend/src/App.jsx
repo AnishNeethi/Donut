520425c6d01c391ae05e299f8fbd697532cc58ca
Anish Neethi Ganesh <141767969+AnishNeethi@users.noreply.github.com>
2025-06-21 19:41:34 -0400

    added landing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import './App.css'
 function App() {
   return (
     <Router>
+      <Landing />
       <div className="App">
         <nav style={{ padding: '20px', backgroundColor: '#f0f0f0' }}>
           <Link to="/" style={{ marginRight: '20px', textDecoration: 'none', color: '#333' }}>
