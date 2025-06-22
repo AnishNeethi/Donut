97caa651e2f616316a35ac4bff420bb4cf821b97
sinndu <sindusaram@gmail.com>
2025-06-22 03:58:09 -0400

    Stuff

--- frontend/src/components/HamburgerMenu.jsx
+++ frontend/src/components/HamburgerMenu.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './HamburgerMenu.css';
 
-const HamburgerMenu = ({ isOpen, onClick }) => {
+const HamburgerMenu = ({ isOpen, toggleMenu }) => {
   return (
-    <button className={`hamburger-menu ${isOpen ? 'menu-open' : ''}`} onClick={onClick}>
+    <button className={`hamburger-menu ${isOpen ? 'menu-open' : ''}`} onClick={toggleMenu}>
       <div className="hamburger-line"></div>
       <div className="hamburger-line"></div>
       <div className="hamburger-line"></div>
